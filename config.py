d5b79fe31fc891ff21312818fc39d72bfbe1041c
Mark007-R <markrodrigues2004@gmail.com>
2026-03-03 00:38:41 +0530

    dead spaces removed

--- config.py
+++ config.py
@@ -30,38 +30,38 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-change-in-production-2026'
     DEBUG = False
     TESTING = False
-    
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_POOL_SIZE = 10
     SQLALCHEMY_POOL_RECYCLE = 3600
-    
+
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'uploads'
     DATASET_FOLDER = os.environ.get('DATASET_FOLDER') or 'datasets'
     VECTOR_DB_FOLDER = os.environ.get('VECTOR_DB_FOLDER') or 'vector_db'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     ALLOWED_EXTENSIONS = {'csv', 'txt'}
-    
+
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     PERMANENT_SESSION_LIFETIME = timedelta(hours=24)
     WTF_CSRF_ENABLED = True
     WTF_CSRF_TIME_LIMIT = None
-    
+
     CACHE_TYPE = 'simple'
     CACHE_DEFAULT_TIMEOUT = 300
-    
+
     MAX_SCRAPE_REVIEWS = int(os.environ.get('MAX_SCRAPE_REVIEWS', 50))
     SCRAPE_TIMEOUT = int(os.environ.get('SCRAPE_TIMEOUT', 30))
-    
+
     MIN_REVIEW_LENGTH = 10
     MAX_KEYWORDS = 8
     TOP_K_RAG = 3
-    
+
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
     LOG_FILE = os.environ.get('LOG_FILE', 'app.log')
-    
+
     GOOGLE_PLACES_API_KEY = os.environ.get('GOOGLE_PLACES_API_KEY', '')
 
 
@@ -82,10 +82,10 @@ class ProductionConfig(Config):
         _mysql_uri_from_env('PROD_') or\
         _mysql_uri_from_env() or\
         'sqlite:///reviews.db'
-    
+
     SESSION_COOKIE_SECURE = True
     WTF_CSRF_ENABLED = True
-    
+
     SQLALCHEMY_POOL_SIZE = 20
     CACHE_TYPE = 'simple'
 
@@ -107,5 +107,5 @@ config = {
 def get_config(env=None):
     if env is None:
         env = os.environ.get('FLASK_ENV', 'development')
-    
+
     return config.get(env, config['default'])
